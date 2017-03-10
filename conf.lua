c0b43dcbd9027433ca1ef6f14226061840e4d58f
stersylva <stersylva@gmail.com>
2017-03-10 14:24:06 -0300

    inclusao de imagens

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.version = "0.10.1" -- a versão do love que eu estou ultizando
+  t.window.title = "Breakout" -- titulo do jogador
+  t.window.width = 1000 --- a largura da telas
+  t.window.height = 600 ---largura da telas
+  t.window.borderless = false -- é a borda da janela pra deixar sem a borada coloca true
+
+end
